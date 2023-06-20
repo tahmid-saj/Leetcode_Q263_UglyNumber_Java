8dc6714e76d36dd6f852d786e9690c8cf46e152a
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-06-20 00:42:02 -0400

    Create q263.java

---
+++ q263.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if (n == 1) {
+            return true;
+        } else if (n == 0) {
+            return false;
+        }
+
+        while (n % 2 == 0 || n % 3 == 0 || n % 5 == 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            }
+
+            if (n % 3 == 0) {
+                n /= 3;
+            }
+
+            if (n % 5 == 0) {
+                n /= 5;
+            }
+        }
+
+        if (n != 1) {
+            return false;
+        }
+
+        return true;
+    }
+}
